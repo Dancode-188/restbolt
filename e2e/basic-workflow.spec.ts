52828b5ef54e84023122072025e2dbeb6cd480d8
biocodeit <glencoelho135@gmail.com>
2025-10-31 10:46:15 +0530

    make basic test more reliable

--- e2e/basic-workflow.spec.ts
+++ e2e/basic-workflow.spec.ts
@@ -4,5 +4,6 @@ test('checking for basic workflow functionality', async ({page}) => {
   await page.goto('/')
   await page.getByPlaceholder('https://api.example.com/endpoint').fill('https://jsonplaceholder.typicode.com/todos/1')
   await page.getByRole('button', {name:'Send'}).click()
-  await expect(page.getByRole('presentation')).toContainText("{  \"userId\": 1,  \"id\": 1,  \"title\": \"delectus aut autem\",  \"completed\": false}")
-})
+  await expect(page.getByRole('presentation')).toContainText('"userId": 1')
+  await expect(page.getByRole('presentation')).toContainText('"id": 1')
+  await expect(page.getByRole('presentation')).toContainText('"title"')})
