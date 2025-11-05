c9b0d9f841c633514e1df20c29dbf29ae2093bb2
biocodeit <glencoelho135@gmail.com>
2025-11-05 15:50:25 +0530

    update collection-test

--- e2e/collection-test.spec.ts
+++ e2e/collection-test.spec.ts
@@ -7,4 +7,14 @@ test('check creation of new collection', async ({page}) => {
     await page.getByPlaceholder('Collection name').fill('New Test Collection')
     await page.getByRole('button',{name: 'Create'}).click()
     await expect(page.getByText('New Test Collection')).toBeVisible()
+    //can use object model here
+    await page.getByPlaceholder('https://api.example.com/endpoint').fill('https://jsonplaceholder.typicode.com/todos/1')
+    await page.getByRole('button', {name:'Send'}).click()
+    await page.getByTitle('Save to collection').click()
+    await page.getByPlaceholder('GET https://jsonplaceholder.typicode.com/todos/1')
+              .fill('Post 1')
+    await page.getByRole('button', {name:'New Test Collection 0 requests'}).click()
+    await page.getByRole('button', { name: '▶ New Test Collection (1)' }).click()
+    await expect(page.getByText('Post 1')).toBeVisible()
+    
 })
