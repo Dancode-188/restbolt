26c167921489f41eaa32dd9b19b9abf29e2e3a8a
biocodeit <glencoelho135@gmail.com>
2025-11-05 15:50:25 +0530

    update collection-test

--- e2e/collection-test.spec.ts
+++ e2e/collection-test.spec.ts
@@ -16,4 +16,5 @@ test('check creation of new collection', async ({page}) => {
     await page.getByRole('button', {name:'New Test Collection 0 requests'}).click()
     await page.getByRole('button', { name: '▶ New Test Collection (1)' }).click()
     await expect(page.getByText('Post 1')).toBeVisible()
+
 })
