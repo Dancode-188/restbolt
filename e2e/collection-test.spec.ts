9d0c18b135d9cfeaf8d0461a5758d17d25b32094
biocodeit <glencoelho135@gmail.com>
2025-11-15 11:15:48 +0530

    resolve issue for collection failure

--- e2e/collection-test.spec.ts
+++ e2e/collection-test.spec.ts
@@ -1,26 +1,21 @@
-import {test, expect } from '@playwright/test'
-import { CollectionModel } from './object-models/collection-model'
-import { APImodel } from './object-models/api-model'
+import {test, expect} from './fixtures/collection' 
 
-test('check creation of new collection', async ({page}) => {
+test('check creation of new collection', async ({collection, apiReq ,page}) => {
     await page.goto("/")
 
     //1. make new collection
-    const collect = new CollectionModel(page)
-    await collect.createCollection('New Test Collection')
+    await collection.createCollection('New Test Collection')
     await expect(page.getByText('New Test Collection')).toBeVisible()
 
     //2. create a new request for collection
-    const request = new APImodel(page)
-    await request.get('https://jsonplaceholder.typicode.com/todos/1')
+    await apiReq.get('https://jsonplaceholder.typicode.com/todos/1')
 
     //3. Save the request to collection
-    await collect.saveToCollection('Post 1', 'New Test Collection')
+    await collection.saveToCollection('Post 1', 'New Test Collection')
 
     //4. Check if the request got saved in the collection
     await page.locator('div')
               .filter({has: page.getByTitle('Delete collection')})
-            //   .filter({hasText: '▼'}).last().click()
-            .getByRole('button').filter({hasText:'New Test Collection'}).click()
+              .getByRole('button').filter({hasText:'New Test Collection'}).click()
     await expect(page.getByText('Post 1')).toBeVisible()
 })
