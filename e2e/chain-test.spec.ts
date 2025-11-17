439a999d9d51446179689a4cb828e210b4f92a62
biocodeit <glencoelho135@gmail.com>
2025-11-17 15:51:53 +0530

    Revert "add chain fixtures to reduce code duplication"
    This reverts commit 40f3d1a0265eb43833624f76f30d27e60862ca4a.

--- e2e/chain-test.spec.ts
+++ e2e/chain-test.spec.ts
@@ -1,25 +1,64 @@
-import {test, expect} from './fixtures/chain.fixtures'
+import {expect, test} from '@playwright/test'
+import { ChainModel } from './object-models/chain-object'
 
-test('checking chain execution', async ({singleChain}) => {
-    await expect(await singleChain.execute('User Chain')).toContainText('COMPLETED')   
+
+test('checking chain execution', async ({page}) => {
+    // 1. go to page
+    await page.goto('/')
+    // 2. add new chain
+    const chain = new ChainModel(page)
+    await chain.addNewChain('User Chain')
+    // 3. add steps to chain
+    await chain.addNewStep('Step 1',1, 'https://jsonplaceholder.typicode.com/users/1')
+    await chain.addExtraction('userId', 1)
+    await chain.addNewStep('Step 2', 2, 'https://jsonplaceholder.typicode.com/posts?userId={{userId}}')
+    await chain.save()
+    //4. check if collection saved
+    await expect(await chain.execute('User Chain')).toContainText('COMPLETED')
+    
 })
 
-test('can user delete chain', async ({singleChain}) => {
-    await singleChain.execute('User Chain')
-    await singleChain.delete('User Chain')
-    await expect(await singleChain.getCollection('User Chain')).toBeVisible({visible:false})
+test('can user delete chain', async ({page}) => {
+     await page.goto('/')
+    // 2. add new chain
+    const chain = new ChainModel(page)
+    await chain.addNewChain('Demo User Chain')
+    // 3. add steps to chain
+    await chain.addNewStep('Step 1',1, 'https://jsonplaceholder.typicode.com/users/1')
+    await chain.addExtraction('userId', 1)
+    await chain.addNewStep('Step 2', 2, 'https://jsonplaceholder.typicode.com/posts?userId={{userId}}')
+    await chain.save()
+    await chain.execute('Demo User Chain')
+    await chain.delete('Demo User Chain')
+    await expect(await chain.getCollection('Demo User Chain')).toBeVisible({visible:false})
 })
 
-test('can user add multiple chains', async ({multiChain}) =>{
+test('can user add multiple chains', async ({page}) =>{
+    await page.goto('/')
+    const chain = new ChainModel(page)
     for(let i=1; i<11; i++) {
-        await expect((await multiChain.getCollection(`User Chain ${i}`)).getByText(`User Chain ${i}`)).toBeVisible()
+        await chain.addNewChain(`User Chain ${i}`)
+        await chain.addNewStep('Step 1', 1, `https://jsonplaceholder.typicode.com/users/${i}`)
+        await chain.save()
+    }
+
+    for(let i=1; i<11; i++) {
+        await expect((await chain.getCollection(`User Chain ${i}`)).getByText(`User Chain ${i}`)).toBeVisible()
     }
 })
 
-test('can user delete multiple chains', async ({multiChain}) => {
+test('can user delete multiple chains', async ({page}) => {
+    await page.goto('/')
+    const chain = new ChainModel(page)
     for(let i=1; i<11; i++) {
-        await multiChain.delete(`User Chain ${i}`)
-        await expect((await multiChain.getCollection(`User Chain ${i}`)).getByText(`User Chain ${i}`))
+        await chain.addNewChain(`User Chain ${i}`)
+        await chain.addNewStep(`Step 1`, 1, `https://jsonplaceholder.typicode.com/users/${i}`)
+        await chain.save()
+    }
+
+    for(let i=1; i<11; i++) {
+        await chain.delete(`User Chain ${i}`)
+        await expect((await chain.getCollection(`User Chain ${i}`)).getByText(`User Chain ${i}`))
         .toBeVisible({visible: false})
     }
 })
