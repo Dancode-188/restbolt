113b868060318ef0d8cc4a9649211515d349d9b5
biocodeit <glencoelho135@gmail.com>
2025-11-30 20:54:42 +0530

    add header section to basepage model
    to select the environemnt the button is in the top header section
    created a locator to be used as parent

--- e2e/object-models/BasePage.ts
+++ e2e/object-models/BasePage.ts
@@ -1,33 +1,30 @@
-import {type Page, Locator} from '@playwright/test'
+import { type Page, Locator } from '@playwright/test'
 
 export class BasePage {
-    constructor(protected page:Page) {
-    
-    if (process.env.THROTLE) {
-    (async () => {
-        const context = this.page.context();
-        const cdpSession = await context.newCDPSession(this.page);
-        await cdpSession.send('Emulation.setCPUThrottlingRate', { rate: 6 });
-        
-                 })()
+    constructor(protected page: Page) {
+        if (process.env.THROTLE) {
+            (async () => {
+                const context = this.page.context();
+                const cdpSession = await context.newCDPSession(this.page);
+                await cdpSession.send('Emulation.setCPUThrottlingRate', { rate: 6 });
+
+            })()
         }
     }
 
-    optionSection : Locator = this.page.locator('#_R_jklrlb_')
-    reqBuilderMain  : Locator = this.page.locator('#_R_5klrlb_')
+    optionSection: Locator = this.page.locator('#_R_jklrlb_')
+    reqBuilderMain: Locator = this.page.locator('#_R_5klrlb_')
+    header: Locator = this.page.getByRole('banner')
 
-    protected button(btnName: string): Locator
-    {
-        return this.page.getByRole('button', {name: btnName})
+    protected button(btnName: string): Locator {
+        return this.page.getByRole('button', { name: btnName })
     }
 
-    protected fillBlock (input: string): Locator 
-    {
+    protected fillBlock(input: string): Locator {
         return this.page.getByPlaceholder(input)
     }
 
-    protected async clearall(locator: Locator) 
-    {
+    protected async clearall(locator: Locator) {
         await locator.press('Control+A')
         await locator.clear()
     }
