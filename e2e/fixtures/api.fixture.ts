529d31bba1b0f7c8b64f26cee0e46619d997a138
biocodeit <glencoelho135@gmail.com>
2025-12-03 17:49:13 +0530

    move preheader and prevariable to api fixture from collection fixture

--- e2e/fixtures/api.fixture.ts
+++ e2e/fixtures/api.fixture.ts
@@ -3,7 +3,8 @@ import { APImodel } from '../object-models/api-model'
 
 interface MyFixtures {
     apiReq : APImodel,
-
+    preHeaders : void,
+    preVariables : void
 }
 
 
@@ -12,6 +13,21 @@ export const test = base.extend<MyFixtures>(
         apiReq : async({page},use) => {
             use(new APImodel(page))
         },
+        preVariables : async({page, apiReq},use) => {
+            await apiReq.chainVariableAdd('chainVar1', 'val1')
+            await apiReq.chainVariableAdd('chainVar2', 'val2')
+            await apiReq.scriptVariableAdd('scriptVar1', 'val1')
+            await apiReq.scriptVariableAdd('scriptVar2', 'val2')
+            await use()
+        },
+        preHeaders  : async ({page, apiReq},use) => {
+            const[name1, name2, name3] = ['author1', 'author2','author3']
+            const[value1, value2, value3] = ['val1', 'val2', 'val3']
+            await apiReq.fillHeader(name1, value1)
+            await apiReq.fillHeader(name2, value2)
+            await apiReq.fillHeader(name3, value3)
+            use()
+        }
     }
 )
 
